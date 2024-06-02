f5e0389e3c742f1612ac13344387a204f03cd0d4
Sidney Teodoro Araujo Araujo <sidneyteodoro2002@gmail.com>
2024-06-02 16:32:58 -0300

    Update page.py

--- src/modulo/page.py
+++ src/modulo/page.py
@@ -4,7 +4,7 @@ import streamlit as st
 def page():
     st.set_page_config(
         page_title="Web Scraper Image", # Título
-        page_icon="src\img\ico.png", # Ícone
+        page_icon="img\ico.png", # Ícone
         layout="centered",  # Layout da página
         menu_items={
             'Report a bug': "https://github.com/SidneyTeodoroJr",
